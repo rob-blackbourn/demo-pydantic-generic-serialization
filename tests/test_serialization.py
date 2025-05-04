0a3ee6f3670534a8957bea76bc1e8ac5f7a5851d
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-05-04 07:22:31 +0100

    tidy

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -15,16 +15,15 @@ def test_serialization() -> None:
         date_of_birth=datetime(1990, 1, 1),
         height=1.75
     )
+    user_json_str = serialize_model_to_json_str(user)
+    deserialized_user = deserialize_model_from_json(user_json_str)
+    assert user == deserialized_user
+
     location = Location(
         name='Central Park',
         latitude=Decimal('40.785091'),
         longitude=Decimal('-73.968285')
     )
-
-    user_json_str = serialize_model_to_json_str(user)
-    deserialized_user = deserialize_model_from_json(user_json_str)
-    assert user == deserialized_user
-
     location_json_str = serialize_model_to_json_str(location)
     deserialized_location = deserialize_model_from_json(location_json_str)
     assert location == deserialized_location
@@ -41,16 +40,15 @@ def test_model() -> None:
         date_of_birth=datetime(1990, 1, 1),
         height=1.75
     )
+    user_json_str = user.model_dump_json()
+    deserialized_user = User.model_validate_json(user_json_str)
+    assert user == deserialized_user
+
     location = Location(
         name='Central Park',
         latitude=Decimal('40.785091'),
         longitude=Decimal('-73.968285')
     )
-
-    user_json_str = user.model_dump_json()
-    deserialized_user = User.model_validate_json(user_json_str)
-    assert user == deserialized_user
-
     location_json_str = location.model_dump_json()
     deserialized_location = Location.model_validate_json(location_json_str)
     assert location == deserialized_location
